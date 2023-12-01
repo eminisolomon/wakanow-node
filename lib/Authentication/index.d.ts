de88b8b8d4e5d4b70dc4cdef23e5f11261d68de9
Solomon Olatunji <aotoluwalope@gmail.com>
2023-12-01 21:19:12 +0100

    Delete lib directory

--- lib/Authentication/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { AuthenticationRequestBody, AuthenticationSuccessResponseBody, environment } from "../Types";
-export declare class Authentication {
-    private environment;
-    constructor(environment: environment);
-    generateToken(requestPayload: AuthenticationRequestBody): Promise<AuthenticationSuccessResponseBody>;
-}
